c31a7c9e84ace20fc02e00f0800054791ce3f490
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-13 12:47:28 +0300

    make the user

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,17 @@
-class User {}
+import faker from 'faker';
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.findName();
+    this.location = {
+      lat: +faker.address.latitude(),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
